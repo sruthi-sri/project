5c51b70b0f73bb7a6d44b37d83b8f46685675439
sruthi-sri <sruthikucharlapati@gmail.com>
2026-04-29 15:47:57 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,193 @@
+let name="santhosh";
+let age=17;
+console.log( "Name =",name);
+console.log("Age=", age);
+console.log("Type of age:",typeof age);
+
+let text="Arithmetic operators:" ;  
+console.log(text);
+let inta=10;
+let intb=5;
+console.log("a=",inta);
+console.log("b=",intb);
+
+console.log("a+b=",inta+intb);
+console.log("a-b",inta-intb);
+console.log("a*b",inta*intb);
+console.log("a/b",inta/intb);
+
+let text1="ItemPrice:";
+console.log(text1);
+let intprice=499;
+console.log("Price=",intprice+1);
+
+
+let marks=80;
+console.log("marks:",marks);
+console.log("Subtract 10 in marks=",marks-10);
+
+let x=4;
+let y=3;
+console.log("x=",x);
+console.log("y=",y);
+console.log("x**y:",x**y);
+
+let total=15;
+console.log("Total",total);
+console.log("total is divided by 5",total/5);
+
+let isJavaScriptFun=true;
+console.log("isJavaScriptFun:",isJavaScriptFun);
+console.log("Datatype of true is",typeof isJavaScriptFun );
+
+let firstName="Hari";
+let lastName="varma";
+console.log(firstName+lastName);
+
+let num="25"
+console.log(num);
+console.log(typeof num);
+
+const nums=25;
+console.log(nums);
+console.log(typeof nums);
+
+let compare1=10;
+let compare2=20;
+console.log(compare1>compare2);
+console.log(compare1<compare2);
+console.log(compare1==compare2);
+
+
+console.log(5==5)
+console.log(5===5)
+
+let p=12;
+let q=12;
+console.log(p==q);
+console.log(p===q);
+
+let value=null;
+console.log( typeof value);
+console.log(value);
+
+let aa=15;
+let bb=4;
+let cc=2;
+console.log(aa+bb*cc);
+
+console.log((20+10)/5);
+
+let int=7;
+console.log(int+3);
+console.log(int-3);
+console.log(int*3);
+console.log(int/3);
+
+let city="Nellore";
+let country="India";
+console.log(typeof(city));
+console.log(typeof(country));
+
+ let integer=25;
+ let string="25";
+ console.log(integer==string);
+ console.log(integer===string);
+
+ console.log(typeof "hello");
+ console.log(typeof 123);
+ console.log(typeof true);
+ console.log(typeof undefined);
+
+ let score=95;
+ console.log(score/5);
+
+ let balance=1000;
+ console.log(balance-275);
+
+ let length=12;
+ let breadth=8;
+ console.log(1/2*breadth*length);
+
+ let num1=9;
+ let num2=2;
+ console.log(num1/num2);
+ console.log(num1%num2);
+
+ console.log(15>=10);
+ console.log(15<=10);
+ console.log(20!=18);
+ console.log(20!=="20");
+
+ let temperature=-5;
+ console.log(typeof temperature);
+ console.log(temperature<0);
+
+ let myValue=undefined;
+ console.log(typeof myValue);
+ console.log(myValue==undefined);
+
+ let int1=0;
+ let int2=false;
+ console.log(int1==int2);
+ console.log(int1===int2);
+
+ let hard1=10;
+ let hard2="10";
+ console.log(hard1==hard2);
+ console.log(hard1===hard2);
+
+ 
+ console.log(typeof 100);
+ console.log(typeof "100");
+ console.log(typeof true);
+ console.log(typeof null);
+
+ let add=15;
+ let sub=5;
+ let mul=2;
+ let div=add/sub+mul*3-1;
+ console.log( div);
+ 
+ let pp="50";
+ let qq=25;
+ console.log(p==q);
+ console.log(p===q);
+
+ let totalMarks=450;
+ let subjects=5;
+ console.log(450/5);
+ 
+ let itemPrice=799;
+ let quantity=3;
+ console.log(itemPrice+quantity);
+
+ let seconds=3600;
+ console.log(3600/60);
+
+ let minutes=120;
+ console.log(minutes *2/minutes);
+
+ let aaa=7;
+ let bbb=2;
+ let ccc=(aaa+bbb)*2-3;
+ console.log(ccc)
+
+ let width=15;
+ let height=10;
+ console.log(2*(width+height));
+
+ let number="123";
+ console.log(typeof number);
+ console.log(number==123)
+ console.log(number===123)
+
+ let a1=null;
+ let b1=undefined;
+ console.log(a1==b1);
+ console.log(a1===b1);
+
+ console.log(typeof NaN);
+ console.log(10/"hello");
+ console.log(typeof(10/"hello"));
+
